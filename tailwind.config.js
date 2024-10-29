6485da365b195b1d2768cc13a87d6bd0232e339d
ledtvexpert <trivalve.bakery@trash.q-t-a.uk>
2024-10-29 12:07:53 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        scroll: 'scroll 10s linear infinite',
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
